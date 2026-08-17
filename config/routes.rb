d48ad4768c4f4e752ecc7f96982078424890815e
david-lafontant <lafontant.david@gmail.com>
2026-08-16 23:10:33 -0400

    Generate pages controller with home and about actions and their related views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  get "/about", to: "pages#about", as: :about
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,5 +13,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "pages#home"
 end
