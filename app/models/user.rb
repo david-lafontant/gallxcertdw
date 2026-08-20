62eb4e1d81e1e718e188b066794da7f1509e62df
david-lafontant <lafontant.david@gmail.com>
2026-08-20 16:28:26 -0400

    Fix linting error with rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :lockable, :trackable, :confirmable
  has_many :pictures, dependent: :destroy
-
 end
