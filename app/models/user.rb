0e16d8987a7645e32f5a566bc2f696a317663ba6
david-lafontant <lafontant.david@gmail.com>
2026-08-20 23:37:43 -0400

    Set different roles and add default role when creating user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :lockable, :trackable, :confirmable
  has_many :pictures, dependent: :destroy
+ enum :role, {user:0, admin:1}
+ after_initialize :set_default_role, if: :new_record?
+ def set_default_role
+  self.role ||= :user
+ end
 end
