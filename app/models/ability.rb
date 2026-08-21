c69f04179a7ec9cf267b2d7eccb57b7920094e1b
david-lafontant <lafontant.david@gmail.com>
2026-08-20 23:38:03 -0400

    Define role ability

---
+++ app/models/ability.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    if user.nil?
+      can :read, Item
+    elsif user.admin?
+     can :manage, :all
+    else
+      can [:read, :create], Item
+      can [:update, :destroy], Item, user: user
+      can :manage, User, user: user
+    end
+    # See the wiki for details:
+    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+  end
+end
