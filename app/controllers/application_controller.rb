2deb3bcc32805a4d79f2d02cd6ebcfa4d631688e
david-lafontant <lafontant.david@gmail.com>
2026-08-31 14:13:15 -0400

    Redirect user if unautorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   before_action :authenticate_user!
+  protect_from_forgery
+
+    #Redirects to login for secure resources
+    rescue_from CanCan::AccessDenied do |exception|
+
+      if user_signed_in?
+        flash[:error] = "Not authorized to view this page"
+        session[:user_return_to] = nil
+        redirect_to root_url
+      else
+        flash[:error] = "You must first login to view this page"
+        session[:user_return_to] = request.url
+        redirect_to "/users/sign_in"
+      end
+
+    end
 end
