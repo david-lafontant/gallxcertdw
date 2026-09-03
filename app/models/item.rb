1d9ca9da6f6d24bb6e47c9fa0359cddb8b79d113
david-lafontant <lafontant.david@gmail.com>
2026-09-02 23:18:48 -0400

    Add basic validation to item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :picture
+
+  validates :poster, presence: true
+  validates :price,  presence: true, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :name, , presence: true, uniqueness: true, length: { in: 6..50 }
+  validates :description, , presence: true, length: { in: 6..2000 }
+  validate :validate_picture
+
+  private
+
+  def validate_picture
+    return unless picture.attached?
+
+    errors.add(:picture, 'is too big, it shoulb be less than 5 MB') unless poster.blob.byte_size <= 5.megabyte
+    acceptable_types = ['image/jpeg', 'image/png']
+    return if acceptable_types.include?(picture.content_type)
+
+    errors.add(:poster, 'must be a JPEG or PNG')
+  end
+
+
+
+
+
 end
