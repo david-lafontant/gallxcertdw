5c5f655e7a103e69ae88509e9490dc37137e45a0
david-lafontant <lafontant.david@gmail.com>
2026-09-19 21:23:01 -0400

    redirect to root pat after an item is destroyed

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -56,7 +56,7 @@ class ItemsController < ApplicationController
     @item.destroy!
 
     respond_to do |format|
-      format.html { redirect_to items_path, notice: "Item was successfully destroyed.", status: :see_other }
+      format.html { redirect_to root_path, notice: "Item was successfully destroyed.", status: :see_other }
       format.json { head :no_content }
     end
   end
