4a2b95530b6617a258d92c8a1f45437ec7ec16eb
david-lafontant <lafontant.david@gmail.com>
2026-09-19 22:50:04 -0400

    Update factory user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,12 @@
 FactoryBot.define do
-    factory :user do
+  factory :user do
     sequence(:email) { |n| "user_#{n}@example.com" }
     password { "password123" }
     password_confirmation { "password123" }
+    role {1}
+    confirmed_at { Time.zone.now - 1.hour }
+    confirmation_sent_at { Time.zone.now - 1.hour }
+    confirmation_token { "abcd1234" }
 
-    confirmed_at { Time.current }
-    before(:create) { |user| user.skip_confirmation! }
   end
 end
